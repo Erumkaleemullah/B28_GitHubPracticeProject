570abb9c474fc60f8d575b6e5172f3342feabdd5
erumkaleemullah <erumkashif@hotmail.com>
2023-01-11 12:31:24 -0600

    Test case 004

--- src/cydeo/TestCase004.java
+++ src/cydeo/TestCase004.java
@@ -3,6 +3,7 @@ package cydeo;
 public class TestCase004 {
     public static void main(String[] args) {
         System.out.println("Created test steps");
+        System.out.println("created test");
 
     }
 }
